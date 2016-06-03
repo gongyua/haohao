df0a2d1ad85eb6b38e4594f1e09936f34e8ca563
gongyua <663630042@qq.com>
2016-06-03 17:13:58 +0800

    hahao
    asdfasdf

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  haohao
+//
+//  Created by pajia on 16/6/3.
+//  Copyright © 2016年 pajia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  haohao
+//
+//  Created by pajia on 16/6/3.
+//  Copyright © 2016年 pajia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
